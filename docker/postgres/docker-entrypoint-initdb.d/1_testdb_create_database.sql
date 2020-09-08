2a3d877e1aa1009b8a5c62a21ff0ef2ec8152d52
imo-tikuwa <imotikuwabathclean@gmail.com>
2020-09-08 17:15:25 +0900

    pgcryptoを有効化するシェルを作成
    参考：https://stackoverflow.com/questions/48814922/create-postgres-extension-in-docker-container
    
    pgcryptoのインストールについてはDockerfile内でcontribをまとめてインストールしていたので特に弄る必要なかった
    
    その他、データベース作成、テーブル作成、データ投入を別のファイルに分けた

---
+++ docker/postgres/docker-entrypoint-initdb.d/1_testdb_create_database.sql
@@ -0,0 +1,26 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 9.6.19
+-- Dumped by pg_dump version 9.6.19
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: testdb; Type: DATABASE; Schema: -; Owner: postgres
+--
+
+CREATE DATABASE testdb WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'en_US.utf8' LC_CTYPE = 'en_US.utf8';
+
+
+ALTER DATABASE testdb OWNER TO postgres;
--- docker/postgres/docker-entrypoint-initdb.d/3_testdb_create_table.sql
+++ docker/postgres/docker-entrypoint-initdb.d/3_testdb_create_table.sql
@@ -2,29 +2,6 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 9.6.19
--- Dumped by pg_dump version 9.6.19
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
---
--- Name: testdb; Type: DATABASE; Schema: -; Owner: postgres
---
-
-CREATE DATABASE testdb WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'en_US.utf8' LC_CTYPE = 'en_US.utf8';
-
-
-ALTER DATABASE testdb OWNER TO postgres;
-
 \connect testdb
 
 SET statement_timeout = 0;
@@ -39,14 +16,14 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner:
 --
 
 CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
 
 --
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner:
 --
 
 COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
